4be77cbe983073223b1e405549f2110f6a238c77
Benjamin Scott <benjamin.scottt.dev@gmail.com>
2024-10-11 13:56:27 -0600

    feat: create feeling
    refactor: Result reuse

--- lib/glare/ux-metrics/ux_metrics.rb
+++ lib/glare/ux-metrics/ux_metrics.rb
@@ -65,17 +65,77 @@ module Glare
           end
         end
       end
+    end
 
-      class Result
-        def initialize(result:, threshold:, label:)
-          @result = result
-          @threshold = threshold
-          @label = label
+    module Feeling
+      class Data
+        CHOICE_KEYS = %w[very_easy somewhat_easy neutral somewhat_difficult very_difficult].freeze
+
+        def initialize(choices:)
+          @choices = choices
         end
 
-        attr_reader :result, :threshold, :label
+        attr_reader :choices
 
+        def valid?
+          if choices.is_a?(Hash) && choices.size
+            missing_attributes = CHOICE_KEYS - choices.keys.map(&:to_s)
+            return true if missing_attributes.empty?
+          end
+
+          false
+        end
+
+        def parse
+          result = choices[:very_easy].to_f +
+            choices[:somewhat_easy].to_f -
+            choices[:neutral].to_f -
+            choices[:somewhat_difficult].to_f -
+            choices[:very_difficult].to_f
+
+          threshold = if result > 1.5
+                        'positive'
+                      elsif result > 1.0
+                        'neutral'
+                      else
+                        'negative'
+                      end
+
+          Result.new(result: result, threshold: threshold, label: threshold)
+        end
+
+        class InvalidDataError < Error
+          def initialize(msg = "Data not valid. Correct data format is: \n\n#{correct_data}")
+            super(msg)
+          end
+
+          def correct_data
+            {
+              choices: {
+                helpful: "string|integer|float",
+                innovative: "string|integer|float",
+                simple: "string|integer|float",
+                joyful: "string|integer|float",
+                complicated: "string|integer|float",
+                confusing: "string|integer|float",
+                overwhelming: "string|integer|float",
+                annoying: "string|integer|float",
+              }
+            }.to_json
+          end
+        end
       end
+    end
+
+    class Result
+      def initialize(result:, threshold:, label:)
+        @result = result
+        @threshold = threshold
+        @label = label
+      end
+
+      attr_reader :result, :threshold, :label
+
     end
   end
 end
